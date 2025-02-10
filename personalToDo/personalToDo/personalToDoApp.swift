55332d0e9114216ee062149c51bf6a55ce91ad9d
Raphael Palacio <54969177+raphaelpalacio@users.noreply.github.com>
2025-02-10 18:17:06 -0500

    initialized project

---
+++ personalToDo/personalToDo/personalToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  personalToDoApp.swift
+//  personalToDo
+//
+//  Created by Raphael Palacio on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct personalToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ personalToDo/personalToDoTests/personalToDoTests.swift
@@ -0,0 +1,17 @@
+//
+//  personalToDoTests.swift
+//  personalToDoTests
+//
+//  Created by Raphael Palacio on 2/10/25.
+//
+
+import Testing
+@testable import personalToDo
+
+struct personalToDoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
